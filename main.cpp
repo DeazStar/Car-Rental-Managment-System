06841b0c697b45c2ec90bdc62a21c5c8372d8c2e
DeazStar <deadfromtheborn@gmail.com>
2022-08-27 11:48:32 +0300

    adding main.cpp

---
+++ main.cpp
@@ -0,0 +1,401 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string.h>
+#include <iomanip>
+#include <stdbool.h>
+
+using namespace std;
+
+
+//void login_user();
+void signup_user();
+void login_user(bool &access);
+void addcar();
+void viewcar();
+
+int idx = 0;
+string usrfirstname, usrlastname, usremail;
+string username_admin = "admin", admin_password = "1234";
+int main()
+{
+    fstream logsfile;
+
+    bool access = false;
+
+    int age, user_input;
+     cout << "\n\n" ;
+    cout << "\t\t Welcome to DeadStar Car Rental" << endl;
+    cout << "\t\t************************************"<< endl;
+    cout << setw(30) << "         1. Admin Login " << endl;
+    cout << setw(39) << "2. User Log in / Sign up" << endl;
+    cout << "\t\t************************************"<< endl;
+    cout << "\n";
+    cout << "\t\t\t Enter your choice -> ";
+    cin >> user_input;
+
+    if(user_input == 1)
+    {
+        string username_input, input_password;
+        retry:
+        cout << "\t\t************************************"<< endl;
+        cout << setw(32) << "Login" << endl;
+        cout << "\t\t************************************"<< endl;
+        cout << "\n";
+        cout << "\t\tEnter your Username: ";
+        cin >> username_input;
+        cout << "\n";
+        cout << "\t\tEnter Password: ";
+        cin >> input_password;
+
+        if(username_input == username_admin && input_password == admin_password)
+        {
+            adminmenu:
+            cout << "\t\t************************************"<< endl;
+            cout << setw(40) << "Welcom Admin" << endl;
+            cout << "\t\t************************************"<< endl;
+            cout <<"\t\t-------------------------------------"<<endl;
+            cout <<"\t\t|"<<setw(33)<<"1. Add Cars                |"<<endl;
+            cout <<"\t\t|"<<setw(33)<<"2. View Cars               |"<<endl;
+            cout <<"\t\t|"<<setw(33)<<"3. Delete Cars             |"<<endl;
+            cout <<"\t\t|"<<setw(33)<<"4. Update Cars             |"<<endl;
+            cout <<"\t\t|"<<setw(33)<<"5. Exit                    |"<<endl;
+            cout <<"\t\t-------------------------------------"<<endl;
+            cout << "\n\t\tEnter your choice -> ";
+            cin >> user_input;
+
+            switch(user_input)
+            {
+                case 1:
+                    addcar();
+                    goto adminmenu;
+                    break;
+                case 2:
+                    viewcar();
+                    goto adminmenu;
+                    break;
+                    
+            }
+
+        }
+        else
+        {
+            cout << "\t\tAccess Denied" << endl;
+            cout << "\n\t\tPress 1 to retry, Press any key to exit... ";
+            cin >> user_input;
+
+            if(user_input == 1)
+            {
+                goto retry;
+            }
+            else
+            {
+                exit(1);
+            }
+        }
+    }
+
+    manu:
+    cout << "\n\n" ;
+    cout << "\t\t Welcome to DeadStar Car Rental" << endl;
+    cout << "\t\t************************************"<< endl;
+    cout << setw(30) << "1. Log in " << endl;
+    cout << setw(30) << "2. Sign up" << endl;
+    cout << "\t\t************************************"<< endl;
+    cout << "\n";
+    cout << "\t\t\t Enter your choice -> ";
+    cin >> user_input;
+
+    switch(user_input)
+    {
+        case 1:
+           login_user(access);
+
+           if(access == true)
+           {
+                cout << "Access Granted " << endl;
+           }
+           else
+           {
+                goto manu;
+           }
+            break;
+        case 2:
+            signup_user();
+            break;
+        default:
+            cout << "\t\tPlease Enter the correct input... "<<endl;
+    }
+
+    return 0;
+
+}
+
+void signup_user()
+{
+    int user_input;
+
+    fstream signups;
+
+    string first_name, last_name, username, email, password;
+    
+    int age;
+
+    string usr;
+
+    cout <<'\n';
+    cout << "\t\t************************************"<< endl;
+    cout << setw(32) << "Sign Up" << endl;
+    cout << "\t\t************************************"<< endl;
+    cout << "\n";
+    cout << "\t\tEnter your first name: ";
+    cin >> first_name;
+    cout << "\n";
+    cout << "\t\tEnter your last name: ";
+    cin >> last_name;
+    cout << "\n";
+    cout << "\t\tEnter your Email: ";
+    cin >> email;
+    cout << "\n";
+    retry:
+    cout << "\t\tEnter user name: ";
+    cin >> username;
+    cout << "\n";
+    cout <<"\t\tEnter password: ";
+    cin >> password;
+
+    signups.open("logsfile.txt", ios::in);
+
+    if(signups.is_open() == false)
+    {
+        cout << "\t\t Unable to access the file at the moment please try again later! " << endl;
+
+    }
+    else
+    {
+        while(getline(signups, usr))
+        {
+            if(username == usr)
+            {
+                cout << "\n";
+                cout << "\t\tThis username is already taken!" << endl;
+                cout << "\n";
+                cout << "\t\ttry anoter username" << endl;
+                cout << "\n";
+                cout << "\t\tpress 1 to try again. press anyother key to exit... ";
+                cin >> user_input;
+
+                if(user_input == 1)
+                {
+                    goto retry;
+                    cout << "\n";
+                }
+                else
+                {
+
+                }
+            }
+            else
+            {
+                idx++;
+            }
+        }
+        
+    }
+
+    idx++;
+
+    signups.close();
+
+    signups.open("logsfile.txt", ios::app);
+
+    if(!signups.is_open())
+    {
+        cout << "\t\t Unable to access the file at the moment please try again later! " << endl;
+
+    }
+    else
+    {
+        signups << username << " " << first_name << " "<< last_name << " "<< email << " " << password << " " << idx << "\n";
+    }
+  
+    signups.close();
+
+
+}
+
+void login_user(bool &access)
+{
+    string username, password,usr, usrname ,usrpassword;
+
+    int user_input;
+
+    int k = 0;
+
+    fstream logs;
+
+    cout << "\t\t************************************"<< endl;
+    cout << setw(32) << "Login" << endl;
+    cout << "\t\t************************************"<< endl;
+    cout << "\n";
+    retry: 
+    cout << "\t\tEnter your Username: ";
+    cin >> username;
+    cout << "\n";
+    cout << "\t\tEnter Password: ";
+    cin >> password;
+
+    logs.open("logsfile.txt", ios::in);
+
+    if(!logs.is_open())
+    {
+        cout << "\t\t Unable to access the file at the moment please try again later! " << endl;
+    }
+    else
+    {
+        while(logs.eof() == false)
+        {
+            logs >> usrname;
+            if(usrname == username)
+            {   
+                logs >> usrfirstname;
+                logs >> usrlastname;
+                logs >> usremail;
+                logs >> usrpassword;
+                logs >> idx;
+
+                if(password == usrpassword)
+                {
+                    access = true;
+                }
+            }
+        }
+    }
+
+    if(access == false)
+    {
+        cout << '\n';
+        cout << "\t\tpassword or username incorrect" << endl;
+        cout << '\n';
+        cout << "\t\tPress 1 to try again, Press any key to exit... ";
+        cin >> user_input;
+        cout << '\n';
+
+        if(user_input == 1)
+        {
+            goto retry;
+        }
+    }
+
+    logs.close();
+
+}
+
+void addcar()
+{
+    fstream cars_info;
+    string manufacture, regno, model;
+    int price, caridx,quantity,quan,file_pointer;
+    char user_input;
+    bool flag = true;
+    string line;
+
+    cout << "\t\t------------------------------------"<< endl;
+    cout << setw(32) << "Add Cars" << endl;
+    cout << "\t\t------------------------------------"<< endl;
+    cout <<"\n";
+    cout << "\t\tEnter RegNO: ";
+    cin.ignore();
+    getline(cin, regno);
+    cout <<"\n\t\tEnter Car Manufacturer: ";
+    getline(cin, manufacture);
+    cout << "\n\t\tEnter Car Model: ";
+    getline(cin, model);
+    cout <<"\n\t\tEnter Quantity: ";
+    cin >> quantity;
+    cout << "\n\t\tEnter Price per day: ";
+    cin >> price;
+
+    cars_info.open("cars.txt", ios::in);
+
+    if(!cars_info.is_open())
+    {
+        cout << "\t file could not be opened at the momement" << endl;
+        cout << "\n\t\tPress any key to continue... ";
+        cin >> user_input;
+        exit(1);
+    }
+    else
+    {
+        // fix this
+        while(getline(cars_info, line))
+        {
+            if(line == manufacture)
+            {
+                if(line == model)
+                {
+                    flag = false;
+                    cars_info >> quan;
+                    file_pointer = cars_info.tellg();
+                    cars_info.seekg(file_pointer, ios::beg);
+                    quantity += quan;
+                }
+            }
+        }
+        // end of fix
+    }
+
+    cars_info.close();
+
+    if(flag == true)
+    {
+        cars_info.open("cars.txt", ios::app);
+        {
+            if(cars_info.is_open())
+            {
+                cars_info << regno << " " << manufacture << " " << model << " "<< quantity << " " << price << endl;
+            }
+        }
+    }
+
+    cars_info.close();
+    
+    
+
+}
+
+void viewcar()
+{
+    char user_input;
+    fstream view_cars;
+    int regno, price, quantity;
+    string manufacture, model;
+
+    view_cars.open("cars.txt", ios::in);
+
+    if(!view_cars.is_open())
+    {
+        cout << "\t\tUnable to access the file at the moment...!";
+        exit(1);
+    }
+    else
+    { 
+        cout <<"\t\t----------------------------------------------------------------------------------" << endl;
+        cout << "\t\t   RegNo  |    Manufacture      |       Model       |     quantity   |   Price   |" << endl;
+        cout <<"\t\t----------------------------------------------------------------------------------" << endl;
+        while(view_cars.eof() == false)
+        {
+            view_cars >> regno >> manufacture >> model >> quantity >> price;
+           
+            cout << "\t\t      " << regno << setw(15) << manufacture << setw(25) << model << setw(15) << quantity << setw(15) << price << endl;
+
+        }
+        cout <<"\t\t----------------------------------------------------------------------------------" << endl;
+
+    }
+
+    view_cars.close();
+
+    cout << "Press any key to continue... ";
+    cin >> user_input;
+}
